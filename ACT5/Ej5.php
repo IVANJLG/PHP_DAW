54c7f9bbab84e04b1430e5320dfe9a05ad08a51e
IVANJLG <Ivanjl849@gmail.com>
2022-09-24 20:40:39 +0200

    entrega

---
+++ ACT5/Ej5.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $diametro = $_POST["diametro"];
+        $altura = $_POST["altura"];
+        $caudal = $_POST["caudal"];
+        /*volumen del cilindro en metros cubicos. 1dm cubico = 1 litro => 0.001m cubico = 1L. caudal = x L/min
+            por tanto si el caudal es x L/min se llenan 0.00x m / min. volumen / 0.00x = minutos. 
+        */
+        $volumen = pi()*($diametro/2)*($diametro/2)*$altura;
+        /*calculo el tiempo en segundos y luego filtrando el resultado por 2 bucles lo transformo en 3 variables. 
+        1 de horas, otra de minutos y otra de segundos*/
+        $segundos = ($volumen / ($caudal/1000))*60;
+        $minutos = 0;
+        $horas = 0;
+        while($segundos > 59){
+            $segundos = $segundos / 60;
+            $minutos = $minutos+1;
+        }
+        while($minutos > 59){
+            $minutos = $minutos / 60;
+            $horas = $horas+1;
+        }
+    ?>
+    <table border="1px solid black">
+        <tr>
+            <th colspan="3">TIEMPO</th>
+        </tr>
+        <tr>
+            <th>HORAS</th>
+            <th>MINUTOS</th>
+            <th>SEGUNDOS</th>
+        </tr>
+        <tr>
+            <th><?php echo $horas?></th>
+            <th><?php echo $minutos?></th>
+            <th><?php echo $segundos?></th>
+        </tr>
+    </table>
+</body>
+</html>
