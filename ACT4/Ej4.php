0e2b361462296e80d8d4b5f019203d21fc61d608
IVANJLG <Ivanjl849@gmail.com>
2022-09-24 19:55:59 +0200

    ACT4

--- ACT4/Ej4.php
+++ ACT4/Ej4.php
@@ -7,6 +7,27 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+        $precio1 = $_POST["precio1"];
+        $precio2 = $_POST["precio2"];
+        $precio3 = $_POST["precio3"];
+        $media = ($precio1 + $precio2 + $precio3)/3;
+        if($precio1 > $media){
+            $diferencia1 = $precio1 - $media;
+        }else{
+            $diferencia1 = $media - $precio1;
+        }
+        if($precio2 > $media){
+            $diferencia2 = $precio2 - $media;
+        }else{
+            $diferencia2 = $media - $precio2;
+        }
+        if($precio3 > $media){
+            $diferencia3 = $precio3 - $media;
+        }else{
+            $diferencia3 = $media - $precio3;
+        }
+    ?>
     <table border="1px solid black">
         <tr><th colspan="3">CALCULADORA DE MEDIAS</th></tr>
         <tr>
@@ -16,19 +37,23 @@
         <tr>
         <tr>
             <th>TIENDA 1</th>
-            <th><th>
+            <th><?php echo $precio1?></th>
+            <th><?php echo $diferencia1?></th>
         </tr>
         <tr>
             <th>TIENDA 2</th>
-            <th><th>
+            <th><?php echo $precio2?></th>
+            <th><?php echo $diferencia2?></th>
         </tr>
         <tr>
             <th>TIENDA 3</th>
-            <th><th>
+            <th><?php echo $precio3?></th>
+            <th><?php echo $diferencia3?></th>
         </tr>
         <tr>
             <th>MEDIA TOTAL</th>
-            <th><th>
+            <th><?php echo $media?></th>
+            <th></th>
         </tr>
     </table>
 </body>
