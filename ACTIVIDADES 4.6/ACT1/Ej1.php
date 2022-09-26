0ba31c60e2865259002555c2cb9f052dd52e5efa
IVANJLG <Ivanjl849@gmail.com>
2022-09-26 13:51:56 +0200

    EJ1 COMPLETO

--- ACTIVIDADES 4.6/ACT1/Ej1.php
+++ ACTIVIDADES 4.6/ACT1/Ej1.php
@@ -14,42 +14,42 @@
                     echo "enhorabuena! Has adivinado el personaje";
                 }else{ echo "respuesta incorrecta";}
     }else{
-        //aqui es donde tienes que mostrar la imagen segun haya hecho click en una o en otra
-        /*
-        if (variable que recoge de la imagen html == 1){ imagen1 = 1.jpg;}else{imagen1=oculto.jpg;}
-        if (variable que recoge de la imagen html == 2){ imagen2 = 2.jpg;}else{imagen1=oculto.jpg;}
-        if (variable que recoge de la imagen html == 3){ imagen3 = 3.jpg;}else{imagen1=oculto.jpg;}
-        if (variable que recoge de la imagen html == 4){ imagen4 = 4.jpg;}else{imagen1=oculto.jpg;}
-        if (variable que recoge de la imagen html == 5){ imagen5 = 5.jpg;}else{imagen1=oculto.jpg;}
-        if (variable que recoge de la imagen html == 6){ imagen6 = 6.jpg;}else{imagen1=oculto.jpg;}
-        if (variable que recoge de la imagen html == 7){ imagen7 = 7.jpg;}else{imagen1=oculto.jpg;}
-        if (variable que recoge de la imagen html == 8){ imagen8 = 8.jpg;}else{imagen1=oculto.jpg;}
-        if (variable que recoge de la imagen html == 9){ imagen9 = 9.jpg;}else{imagen1=oculto.jpg;}
-
-        
-        */
-    }
-        
+        $num = $_REQUEST["num"];
+        if($num==1){$imagen1 = "Images/1.jpg";}else{$imagen1 = "Images/oculto.jpg";}
+        if($num==2){$imagen2 = "Images/2.jpg";}else{$imagen2 = "Images/oculto.jpg";}
+        if($num==3){$imagen3 = "Images/3.jpg";}else{$imagen3 = "Images/oculto.jpg";}
+        if($num==4){$imagen4 = "Images/4.jpg";}else{$imagen4 = "Images/oculto.jpg";}
+        if($num==5){$imagen5 = "Images/5.jpg";}else{$imagen5 = "Images/oculto.jpg";}
+        if($num==6){$imagen6 = "Images/6.jpg";}else{$imagen6 = "Images/oculto.jpg";}
+        if($num==7){$imagen7 = "Images/7.jpg";}else{$imagen7 = "Images/oculto.jpg";}
+        if($num==8){$imagen8 = "Images/8.jpg";}else{$imagen8 = "Images/oculto.jpg";}
+        if($num==9){$imagen9 = "Images/9.jpg";}else{$imagen9 = "Images/oculto.jpg";}
     ?>
+        <table>
+            <tr> 
+                <th><img src=<?php echo $imagen1?>></th> 
+                <th><img src=<?php echo $imagen2?>></th> 
+                <th><img src=<?php echo $imagen3?>></th> 
+            </tr>
 
-            <table>
-                <tr> 
-                    <th><img src="Images/oculto.jpg"></th> 
-                    <th><img src="Images/oculto.jpg"></th> 
-                    <th><img src="Images/oculto.jpg"></th> 
-                </tr>
+            <tr> 
+                <th><img src=<?php echo $imagen4?>></th> 
+                <th><img src=<?php echo $imagen5?>></th> 
+                <th><img src=<?php echo $imagen6?>></th> 
+            </tr>
 
-                <tr> 
-                    <th><img src="Images/oculto.jpg"></th> 
-                    <th><img src="Images/oculto.jpg"></th> 
-                    <th><img src="Images/oculto.jpg"></th> 
-                </tr>
+            <tr> 
+                <th><img src=<?php echo $imagen7?>></th> 
+                <th><img src=<?php echo $imagen8?>></th> 
+                <th><img src=<?php echo $imagen9?>></th> 
+            </tr>
+        </table>
 
-                <tr> 
-                    <th><img src="Images/oculto.jpg"></th> 
-                    <th><img src="Images/oculto.jpg"></th> 
-                    <th><img src="Images/oculto.jpg"></th> 
-                </tr>
-            </table>
+        <form>
+            <input type="text" name="personaje">
+            <input type="submit" value="Comprobar">
+        </form>
+    
+    <?php header('Refresh:1; url=Ej1.html'); }?>
     </body>
 </html>
