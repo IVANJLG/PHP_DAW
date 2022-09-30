da7c0fda6a912c0ffd0bedf3892c8060f34f90c9
IVANJLG <Ivanjl849@gmail.com>
2022-09-30 08:34:17 +0200

    Ej5

---
+++ ACTIVIDADES 4.6/ACT5/Ej5-a.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    img{
+        width:40px;
+    }
+</style>
+<body>
+    <table>
+        <?php
+            for($i=0;$i<=10;$i++){
+                echo "<tr>";
+                for($j=0;$j<=10;$j++){
+                    echo "<th><a href='Ej5-b.php?i=".$i."&j=".$j."'><img src='Images/ojocerrado.png'></a></th>";
+                }
+                echo "</tr>";
+            }
+        ?>
+    </table>
+    
+</body>
+</html>
---
+++ ACTIVIDADES 4.6/ACT5/Ej5-b.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    img{width:40px;}
+</style>
+<body>
+    <table>
+    <?php
+    header("refresh:2;url=Ej5-a.php");
+        $fila = $_GET["i"];
+        $columna = $_GET["j"];
+
+        for($i=0;$i<=10;$i++){
+            echo "<tr>";
+            for($j=0;$j<=10;$j++){
+                echo "<th>";
+                if($i==$fila && $j==$columna){   
+                    echo "<img src='Images/ojoabierto.png'>";
+                }else{
+                    echo "<img src='Images/ojocerrado.png'>";
+                }
+                echo "</th>";
+            }
+            echo "</tr>";
+        }
+    ?>
+    </table>
+</body>
+</html>
