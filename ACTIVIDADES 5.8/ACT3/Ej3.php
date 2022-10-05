5e4743215ef8eeea668e1e16e171cd903738c09d
IVANJLG <Ivanjl849@gmail.com>
2022-10-05 13:20:35 +0200

    act3 a medias

---
+++ ACTIVIDADES 5.8/ACT3/Ej3.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    table{
+        background:url("Images/gollum.jpg");
+        margin: 0 auto 0 auto;
+        
+    }
+
+    img{
+        width:50px;
+        margin:-2px;
+        margin-bottom:-4px;
+    }
+</style>
+<body>
+    <?php
+        if(!isset($_GET["solucion"])){
+            $solucion = [];
+            for($i=0 ; $i<100 ; $i++){
+                $solucion[$i] = 0;
+            }
+        }else{
+            $solucion = unserialize($_GET["solucion"]);
+            $contador = $_GET["contador"];
+            if($solucion[$contador] == 0){
+                $solucion[$contador] = 1;
+            }else{
+                $solucion[$contador] = 0;
+            }
+        }
+    ?>
+    <table >
+        <?php
+            $contador = 0;
+            for($i=0;$i<10;$i++){
+                echo "<tr>";
+                for($j=0;$j<10;$j++){
+                    if($solucion[$contador] == 0){
+                        echo "<th>
+                                <a href='Ej3.php?solucion=".serialize($solucion)."&contador=".$contador."'>
+                                    <img src='Images/oculto.jpg'>
+                                </a>
+                            </th>";
+                    }else{
+                        echo "<a href='Ej3.php?solucion=".serialize($solucion)."&contador=".$contador."'><th>
+                                 
+                            </th></a>";
+                    }
+                    $contador++;
+                }
+                echo "</tr> ";
+            }
+        ?>
+    </table>
+</body>
+</html>
