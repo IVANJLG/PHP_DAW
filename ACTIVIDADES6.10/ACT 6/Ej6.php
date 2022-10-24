521256a310d78602e321a8bde050d5cb8bf55651
IVANJLG <Ivanjl849@gmail.com>
2022-10-24 10:14:48 +0200

    abc

--- ACTIVIDADES6.10/ACT 6/Ej6.php
+++ ACTIVIDADES6.10/ACT 6/Ej6.php
@@ -15,12 +15,19 @@
         $mes =  $_GET["Mes"];
         $año =  $_GET["Año"];
 
+        $fecha = date_create();
+        echo "FECHA ACTUAL<br>";
+        echo date_format($fecha,"d-F-Y")."<br>";
+        echo "FECHA ACTUAL +".$dia." DIAS<br>";
+        $fecha = strtotime("+$dia days +$mes month +$año years");
+        echo date("d-F-Y",$fecha)."<br>";
+        echo "Y corresponde al ".date("l",$fecha);
         
     }else{
         echo "
         <table border='1px solid black'>
             <form method='get' action='Ej6.php'>
-                <tr><th colspan=6>Numero de dias, meses y años transcurridos tras la fecha actual</th></tr>
+                <tr><th colspan=3>Numero de dias, meses y años transcurridos tras la fecha actual</th></tr>
                 <tr>
                     <th><input type='number' name='Dia' min='1' max='31' placeholder='Dia' required></th>
                     <th><input type='number' name='Mes' min='1' max='12' placeholder='Mes' required></th>
--- ACTIVIDADES6.10/ACT 7/Ej7.php
+++ ACTIVIDADES6.10/ACT 7/Ej7.php
@@ -10,13 +10,18 @@
     <?php
     /*Pedir al usuario su fecha de nacimiento y una fecha futura, y mostrar la edad que tendrá en esa
     fecha (un año tiene 60*60*24*365.25 segundos)*/
-    if(isset($_GET["Dia"])){
-        $dia =  $_GET["Dia"];
-        $mes =  $_GET["Mes"];
-        $año =  $_GET["Año"];
+    if(isset($_GET["DiaNac"])){
+        $diaNac =  $_GET["DiaNac"];
+        $MesNac =  $_GET["MesNac"];
+        $AñoNac =  $_GET["AñoNac"];
+        $DiaF =  $_GET["DiaF"];
+        $MesF =  $_GET["MesF"];
+        $AñoF =  $_GET["AñoF"];
 
-        if(checkdate($mes,$dia,$año)){
-            
+        if(checkdate($MesNac,$diaNac,$AñoNac) && checkdate($MesF,$DiaF,$AñoF)){
+            $fecha = date_Create($diaNac."/".$MesNac."/".$AñoNac);
+            $fecha = date_Create($DiaF."/".$MesF."/".$AñoF);
+
         }else{
             echo "FECHA INCORRECTA. VUELVA A INTENTARLO.";
         }
@@ -24,12 +29,21 @@
         echo "
         <table border='1px solid black'>
             <form method='get' action='Ej7.php'>
+                <tr><th colspan=3>Fecha de nacimiento</th></tr>
                 <tr>
-                    <th><input type='number' name='Dia' min='1' max='31' placeholder='Dia' required></th>
-                    <th><input type='number' name='Mes' min='1' max='12' placeholder='Mes' required></th>
-                    <th><input type='number' name='Año' min='1' max='10000' placeholder='Año' required></th>
+                    <th><input type='number' name='DiaNac' min='1' max='31' placeholder='Dia' required></th>
+                    <th><input type='number' name='MesNac' min='1' max='12' placeholder='Mes' required></th>
+                    <th><input type='number' name='AñoNac' min='1' max='10000' placeholder='Año' required></th>
                 </tr>
-                <tr><th colspan='4'><input type='submit'></th></tr>
+                <tr><th colspan=3>Fecha futura</th></tr>
+                <tr>
+                    <th><input type='number' name='DiaF' min='1' max='31' placeholder='Dia' required></th>
+                    <th><input type='number' name='MesF' min='1' max='12' placeholder='Mes' required></th>
+                    <th><input type='number' name='AñoF' min='1' max='10000' placeholder='Año' required></th>
+                </tr>
+                <tr>
+                    <th colspan='4'><input type='submit'></th>
+                </tr>
             </form>
         </table>
         ";
