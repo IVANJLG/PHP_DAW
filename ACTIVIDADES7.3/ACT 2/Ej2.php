e208382dbc58b6ddd8c3066f9db47ca7ec875e48
IVANJLG <Ivanjl849@gmail.com>
2022-10-26 09:38:41 +0200

    abc

--- ACTIVIDADES7.3/ACT 2/Ej2.php
+++ ACTIVIDADES7.3/ACT 2/Ej2.php
@@ -15,7 +15,48 @@
     cómputo. Utiliza sesiones. 
     */
 
-    
+    session_start();
+
+        if(isset($_SESSION["suma"]) && isset($_POST["Numero"]) && isset($_SESSION["MayorPar"])){
+            if($_POST["Numero"]>0 && $_POST["Numero"]%2!=0){
+                $_SESSION["suma"]+=$_POST["Numero"];
+                $_SESSION["contador"]++;
+            }
+            if($_POST["Numero"]>$_SESSION["MayorPar"] && $_POST["Numero"]%2==0){
+                $_SESSION["MayorPar"] = $_POST["Numero"];
+            }
+
+            echo "SUMA NUMEROS IMPARES: ".$_SESSION["suma"]."<br><br>";
+            echo "CONTADOR NUMEROS IMPARES: ".$_SESSION["contador"]."<br><br>";
+            if($_SESSION["MayorPar"]!=0){
+                echo "MAYOR NUMERO PAR: ".$_SESSION["MayorPar"]."<br><br>";
+            }
+           
+        }else{
+            $_SESSION["suma"]=0;
+            $_SESSION["contador"] = 0;
+            $_SESSION["MayorPar"] = 0;
+        }
+
+        if(!isset($_POST["Numero"])){
+            echo "
+                <form method='post' action='Ej2.php'>
+                    Introduce un numero: <input type='number' name='Numero' required><input type='submit'>
+                </form>
+            ";
+        }else{
+            if($_POST["Numero"]>=0){
+                echo "
+                <form method='post' action='Ej2.php'>
+                    Introduce un numero: <input type='number' name='Numero' required><input type='submit'>
+                </form>
+                ";
+            }else{
+                echo "La media de estos numeros es de: ".($_SESSION["suma"]/($_SESSION["contador"]));
+            }
+        }
+
+        /* session_destroy(); */
     ?>
 </body>
 </html>
